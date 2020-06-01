e9a15b90ab7143b3a7b8ff158e58d36e860e4338
Erik De Luna <codingerik1.gmail.com>
2020-06-01 15:01:17 -0500

    second ajax call for search for sound is set up

--- app.js
+++ app.js
@@ -10,41 +10,41 @@ let querySearch;
 
 
 
-// $('#submit').on('click', function () {
-//     querySearch = $('.userInput').val();
-//     console.log('this is the querySearch', querySearch);
+$('#submit').on('click', function () {
+    querySearch = $('.userInput').val();
+    console.log('this is the querySearch', querySearch);
 
 
-//     // this give you the a list of id of a specific query search 
-//     // https://freesound.org/apiv2/search/text/?query=cars&format=api&token=hs78SPDVdG9bSdkeG9sZ6PbBE0rW5vp6ziy1BMvK
+    // this give you the a list of id of a specific query search 
+    // https://freesound.org/apiv2/search/text/?query=cars&format=api&token=hs78SPDVdG9bSdkeG9sZ6PbBE0rW5vp6ziy1BMvK
 
 
-//     // this gets us the previews for each specific sound id that we search 
-//     // https://freesound.org/apiv2/sounds/1234/
+    // this gets us the previews for each specific sound id that we search 
+    // https://freesound.org/apiv2/sounds/1234/
 
 
-//     $.ajax({
-//         url: `https://freesound.org/apiv2/search/text/?format=json&query=${querySearch}&token=${apiKey}`,
-//         method: 'GET'
-//     }).then((response) => {
+    $.ajax({
+        url: `https://freesound.org/apiv2/search/text/?format=json&query=${querySearch}&token=${apiKey}`,
+        method: 'GET'
+    }).then((response) => {
 
-//         console.log(response);
-
-
-//         let id = response.results[0].id
-
-//         // console.log(reponse.results);
-
-//         // for (let i = 0; i < reponse.results.length; i++) {
-//         // console.log(id);
-//         //     // console.log(reponse.results[i].name);
-//         // }
+        console.log(response);
 
 
-//     });
+        let id = response.results[0].id
+
+        // console.log(reponse.results);
+
+        // for (let i = 0; i < reponse.results.length; i++) {
+        // console.log(id);
+        //     // console.log(reponse.results[i].name);
+        // }
+
+
+    });
 
     
-// });
+});
 
 
 
