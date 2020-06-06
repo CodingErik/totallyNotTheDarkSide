7fc132b56ff23c0f6013c2dc2a8798d372a983a5
Dale Kittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-06-05 19:04:54 -0500

    uploading test files

--- spaceflightnews.js
+++ spaceflightnews.js
@@ -1,37 +1,41 @@
-//add on ready function
-//add eventlistener on click from drop down menu
+$(document).ready(function () {
+  //add eventlistener on click from drop down menu
 
-spaceFlightNews();
+  spaceFlightNews();
 
-function spaceFlightNews() {
-  $.ajax({
-    url: `https://spaceflightnewsapi.net/api/v1/articles`,
-    method: "GET",
-  }).then((response) => {
-    for (var i = 0; i < response.docs.length; i++)
-      console.log(
-        response.docs[i].title,
-        response.docs[i].url,
-        response.docs[i].published_date
-      );
+  //this returns a predefined lsit of articles
+  function spaceFlightNews() {
+    $.ajax({
+      url: `https://spaceflightnewsapi.net/api/v1/articles`,
+      method: "GET",
+    }).then((response) => {
+      for (var i = 0; i < response.docs.length; i++)
+        console.log(
+          response.docs[i].title,
+          response.docs[i].url,
+          response.docs[i].published_date
+        );
 
-    // console.log(response.media_type);
-  });
-}
+      // console.log(response.media_type);
+    });
+  }
 
-function searchSpaceFlightNews() {
-  $.ajax({
-    url: `https://spaceflightnewsapi.net/api/v1/articles?search=`, //+ FRONTBOX KEYWORD INPUT
+  document.getElementById("searchBtn").addEventListener("click");
+  // this will add the user defined search term to a search
+  function searchSpaceFlightNews() {
+    $.ajax({
+      url: `https://spaceflightnewsapi.net/api/v1/articles?search=`, //+ FRONTBOX KEYWORD INPUT
 
-    method: "GET",
-  }).then((response) => {
-    for (var i = 0; i < response.docs.length; i++)
-      console.log(
-        response.docs[i].title,
-        response.docs[i].url,
-        response.docs[i].published_date
-      );
+      method: "GET",
+    }).then((response) => {
+      for (var i = 0; i < response.docs.length; i++)
+        console.log(
+          response.docs[i].title,
+          response.docs[i].url,
+          response.docs[i].published_date
+        );
 
-    // console.log(response.media_type);
-  });
-}
+      // console.log(response.media_type);
+    });
+  }
+}); //close on ready
