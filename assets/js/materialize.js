dafed37795cc3f9bcdc959d586a78102bf86f575
Erik De Luna <codingerik1.gmail.com>
2020-06-09 14:45:10 -0500

    added reponsive query search and buttons

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -3,6 +3,21 @@ $(document).ready(function () {
 
     $('.tabs').tabs();
 
+    
+    $('.tabs').on('click', populateQuerySearch)
 
 
+
+    function populateQuerySearch(){
+        
+        // for testing
+        // console.log('hello');
+
+      // toggle hidingQuery on aside for easy access to query search and card
+      $('aside').toggleClass('hidingQuery');
+      
+      // toggle hidingQuery on section for easy access to query search and card
+      $('section').toggleClass('hidingQuery');
+
+    }
 });
