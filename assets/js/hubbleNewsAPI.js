79dbc1222c3ae075cdb8c205655e9a875aebc0f4
Erik De Luna <codingerik1.gmail.com>
2020-06-09 16:21:03 -0500

    set up space flight dynamics and hubble news dynamic functionality

--- assets/js/hubbleNewsAPI.js
+++ assets/js/hubbleNewsAPI.js
@@ -1,7 +1,47 @@
 // hubble News API this lets us the latest news  ******* WORK ON THIS
-{
-    function hubbleAjaxCall(){
 
+$(document).ready(function () {
+
+
+
+
+    $('.hubbleSearch').on('click', function (e) {
+        e.preventDefault();
+        // clear div 
+        $('.newsDiv').empty();
+        // call 
+        hubbleAjaxCall();
+        
+
+        console.log('this is being clicked')
+    })
+
+
+    $('.hubbleClear').on('click', function (e) {
+        e.preventDefault();
+        // empties div for new search 
+        $('.newsDiv').empty();
+    })
+
+
+
+    $.ajaxPrefilter( function (options) {
+        if (options.crossDomain && jQuery.support.cors) {
+          var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+          options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+          //options.url = "http://cors.corsproxy.io/url=" + options.url;
+        }
+      });
+
+
+
+    // hubble glossary 
+    function hubbleAjaxCall() {
+
+
+        
+
+        console.log('call hubble is happening')
         // this is the latest news 
         //http://hubblesite.org/api/v3/news_release/last
 
@@ -9,21 +49,33 @@
         // this is a list of the news 
         // http://hubblesite.org/api/v3/news
 
-        let baseUrl = 'https://hubblesite.org/';
 
+        // user input
+        let userInput = $('#hubbleUserInput').val().trim();
 
         $.ajax({
-            url: 'http://hubblesite.org/api/v3/news_release/last',
+            // url: `https://hubblesite.org/api/v3/glossary/asteroid?callback=json`,
+            url: `http://hubblesite.org/api/v3/glossary/${userInput}`,
             method: 'GET'
-        }).then((response)=> {
+        }).then((response) => {
 
-            console.log(response);
+            // testing user output 
+            // console.log(response.definition);
+
+
+            let definition = $('<div>').text(response.definition).css({
+                padding: '30px'
+            })
+
+           
+
+            $('.newsDiv').append(definition);
 
         })
+
+        
 
 
     }
 
-
-    hubbleAjaxCall();
-}
+});
