0d5745a1d96552245664575825c0ec9c47524a0e
drkittendorf <60675599+drkittendorf@users.noreply.github.com>
2020-06-12 20:45:07 -0500

    fixed placeholder

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -1,34 +1,30 @@
-// adding document .ready SO THAT THIS FIRES ONLY WHEN THE DOCUMENT IS READY 
+// adding document .ready SO THAT THIS FIRES ONLY WHEN THE DOCUMENT IS READY
 $(document).ready(function () {
+  // triggering tabs/slider
+  $(".tabs").tabs();
 
-  // triggering tabs/slider 
-  $('.tabs').tabs();
-
-  // when mouse enter they will be displayed 
-  $('.tabs').mouseenter(populateQuerySearch);
+  // when mouse enter they will be displayed
+  $(".tabs").mouseenter(populateQuerySearch);
 
   // when tab is clicked the newsDiv will clear
-  $('.tabs').on('click', clearNewsDiv);
+  $(".tabs").on("click", clearNewsDiv);
 
-  // when mouse leaves they will no longer be displayed 
+  // when mouse leaves they will no longer be displayed
   // $(".search").mouseleave(closeQuerySearch);
 
-  // populateQuerySearch FUNCTION 
+  // populateQuerySearch FUNCTION
   // ******************************************
   function populateQuerySearch() {
-
-
     // for testing
     // console.log('hello');
 
-    $('.focusNew').attr('placeholder', 'search News');
-    $('.focusHub').attr('placeholder', 'search glossary');
+    $(".focusNew").attr("placeholder", "");
+    $(".focusHub").attr("placeholder", "");
 
     M.updateTextFields();
 
     // toggle hidingQuery on aside for easy access to query search and card
-    $('.search').removeClass('hidingQuery');
-
+    $(".search").removeClass("hidingQuery");
   }
   // ******************************************
 
@@ -36,27 +32,18 @@ $(document).ready(function () {
   // when a new tab is clicked the last input is cleared
   // ******************************************
   function clearNewsDiv() {
-    // empties the previous search before we see the search boxes || buttons 
+    // empties the previous search before we see the search boxes || buttons
     $(".newsDiv").empty();
   }
   // ******************************************
 
-
-
   // closeQuerySearch function
   // this hides the querySearch and the news when the user clicks away from it
-  // click anywhere in the body 
+  // click anywhere in the body
   // ******************************************
   // function closeQuerySearch() {
   // adding the hidingQuery to both search and results when we click away from the are
   // $('.search').addClass('hidingQuery');
   // }
   // ******************************************
-
-
-
-  
 });
-
-
-
